e8117daa425487ae808851599701790c9d9cfd06
James Njenga <knjenga94@gmail.com>
2022-09-23 23:15:40 +0300

    Completed Assignment

---
+++ src/components/About.js
@@ -0,0 +1,12 @@
+function About({image="https://via.placeholder.com/215",myabout}){
+    
+
+return(<aside>
+    <img src={image} alt="blog logo"></img>
+    <p>{myabout}</p>
+</aside>
+);
+
+    
+}
+export default About;
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+
+
+function Header({name}){
+    
+    return(<><h1>{name}</h1> </>);
+     
+
+    
+}
+
+export default Header;
